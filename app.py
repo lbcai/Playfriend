b71bd4a7b3065159c044a80f4be133bbd4e5d90b
lbcai <73155687+lbcai@users.noreply.github.com>
2021-01-18 20:36:23 -0800

    Update webpage to redirect to bot invite link.

--- app.py
+++ app.py
@@ -2,10 +2,26 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+index = """
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Redirecting...</title>
+</head>
+<body>
+<script>
+try { window.location.replace("https://discord.com/oauth2/authorize?client_id=785345529722175498&permissions=470080&scope=bot"); }
+catch(e) { window.location = "https://discord.com/oauth2/authorize?client_id=785345529722175498&permissions=470080&scope=bot"; }
+</script>
+</body>
+</html>
+"""
+
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return index
 
 
 if __name__ == '__main__':
