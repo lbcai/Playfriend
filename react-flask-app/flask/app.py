3fffa5b83066c7463583dfbc531f4285c8eaddfe
L Cai <lizbcai@gmail.com>
2022-06-14 22:52:12 -0700

    Add MongoDB, bot pushes finished games to DB.

--- react-flask-app/flask/app.py
+++ react-flask-app/flask/app.py
@@ -1,24 +1,29 @@
 from flask import Flask, redirect, request
-from flask_mysqldb import MySQL
+from dotenv import load_dotenv
 import os
+import pymongo
+import json
 import time
 
 app = Flask(__name__)
 
-app.config['MYSQL_HOST'] = os.getenv('CLEARDB_DATABASE_URL')
-app.config['MYSQL_USER'] = os.getenv('CLEARDB_USER')
-app.config['MYSQL_PASSWORD'] = os.getenv('CLEARDB_PASS')
-app.config['MYSQL_DB'] = os.getenv('CLEARDB_NAME')
+load_dotenv()
+MONGODB_URI = os.getenv('MONGODB_URI')
+# Connect to database
+client = pymongo.MongoClient(MONGODB_URI)
+mongo_db = client.db
+mongo_db.launches.drop()
 
-mysql = MySQL(app)
 
 @app.route('/')
 def index():
     return redirect("https://discord.com/oauth2/authorize?client_id=785345529722175498&permissions=470080&scope=bot")
 
+
 @app.route('/time')
 def get_current_time():
     return {'time': time.time()}
 
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
--- react-flask-app/flask/playfriend.py
+++ react-flask-app/flask/playfriend.py
@@ -1,8 +1,13 @@
 import os
 import random
+from datetime import date
+
 import discord
 import emojis
 import re
+
+import dns
+import pymongo
 from discord.ext import commands
 from discord.ext.commands import Bot
 from dotenv import load_dotenv
@@ -13,30 +18,32 @@ bot = Bot(command_prefix='>')
 hm_game_dictionary = {}
 ttt_game_dictionary = {}
 
+MONGODB_URI = os.getenv('MONGODB_URI')
+# Connect to database
+client = pymongo.MongoClient(MONGODB_URI)
+mongo_db = client.db
+mongo_db.launches.drop()
+
 
 class Dungeon(commands.Cog):
     """Docs here"""
 
-
-    #class Mage:
+    # class Mage:
     #    def __init__(self):
     #        self.armorType = cloth
 
+    # class Priest:
 
-    #class Priest:
+    # class Warrior:
 
+    # class Archer:
 
-    #class Warrior:
+    # class Thief:
 
-
-    #class Archer:
-
-
-    #class Thief:
-
-    #@commands.command(name='dsetup', help='Start a new Dungeon session.')
-    #async def dungeon_setup(self, ctx):
+    # @commands.command(name='dsetup', help='Start a new Dungeon session.')
+    # async def dungeon_setup(self, ctx):
     #    bot.add_cog(Dungeon())
+
 
 class Tictactoe(commands.Cog):
     """Allows users to play tic tac toe. Use function ttt_start to create game instances.
@@ -62,6 +69,18 @@ class Tictactoe(commands.Cog):
         self.tt_p1_symbol = ttt_game_dictionary[ctx.channel][3]
         self.tt_p2_symbol = ttt_game_dictionary[ctx.channel][4]
         self.ttt_first_player = 0
+
+        if ttt_game_dictionary[ctx.channel][2] is not f'{bot.user.name}':
+            player_2 = ttt_game_dictionary[ctx.channel][2].name + "#" + ttt_game_dictionary[ctx.channel][
+                2].discriminator
+        else:
+            player_2 = f'{bot.user.name}'
+        today_date = date.today()
+        self.game_doc = {'player1': ttt_game_dictionary[ctx.channel][1].name +
+                                    "#" + ttt_game_dictionary[ctx.channel][1].discriminator,
+                         'player2': player_2, 'game': 'Tic-Tac-Toe',
+                         'date': '%s/%s/%s' % (today_date.month, today_date.day,
+                                               today_date.year)}
 
     async def ttt_ai_move(self, ctx):
         tt_list = self.tt_board_list
@@ -163,47 +182,64 @@ class Tictactoe(commands.Cog):
                     return
                 elif tt_list[offset] == 1:
                     await ctx.channel.send(f'{ttt_game_dictionary[ctx.channel][1].mention} has won the game!')
+                    # Winner is player 1
+                    self.game_doc['winner'] = self.game_doc['player1']
+
                 elif tt_list[offset] == 2:
                     if ttt_game_dictionary[ctx.channel][2] is not f'{bot.user.name}':
                         await ctx.channel.send(f'{ttt_game_dictionary[ctx.channel][2].mention} has won the game!')
                     else:
                         await ctx.channel.send('Better luck next time!')
+                    # Winner is player 2
+                    self.game_doc['winner'] = self.game_doc['player2']
                 await Tictactoe.ttt_quit(ttt_game_dictionary[ctx.channel], ctx)
-                return
+                return mongo_db.games.insert_one(self.game_doc)
         for column in range(0, 3):
             if tt_list[column] == tt_list[column + 3] and tt_list[column + 3] == tt_list[column + 6]:
                 if tt_list[column] == 0:
                     return
                 elif tt_list[column] == 1:
                     await ctx.channel.send(f'{ttt_game_dictionary[ctx.channel][1].mention} has won the game!')
+                    # Winner is player 1
+                    self.game_doc['winner'] = self.game_doc['player1']
                 elif tt_list[column] == 2:
                     if ttt_game_dictionary[ctx.channel][2] is not f'{bot.user.name}':
                         await ctx.channel.send(f'{ttt_game_dictionary[ctx.channel][2].mention} has won the game!')
                     else:
                         await ctx.channel.send('Better luck next time!')
+                    # Winner is player 2
+                    self.game_doc['winner'] = self.game_doc['player2']
                 await Tictactoe.ttt_quit(ttt_game_dictionary[ctx.channel], ctx)
-                return
-        if (tt_list[0] == tt_list[4] and tt_list[4] == tt_list[8]) or (tt_list[2] == tt_list[4] and tt_list[4] == tt_list[6]):
+                return mongo_db.games.insert_one(self.game_doc)
+        if (tt_list[0] == tt_list[4] and tt_list[4] == tt_list[8]) or (
+                tt_list[2] == tt_list[4] and tt_list[4] == tt_list[6]):
             if tt_list[4] == 0:
                 return
             elif tt_list[4] == 1:
                 await ctx.channel.send(f'{ttt_game_dictionary[ctx.channel][1].mention} has won the game!')
+                # Winner is player 1
+                self.game_doc['winner'] = self.game_doc['player1']
             elif tt_list[4] == 2:
                 if ttt_game_dictionary[ctx.channel][2] is not f'{bot.user.name}':
                     await ctx.channel.send(f'{ttt_game_dictionary[ctx.channel][2].mention} has won the game!')
                 else:
                     await ctx.channel.send('Better luck next time!')
+                # Winner is player 2
+                self.game_doc['winner'] = self.game_doc['player2']
             await Tictactoe.ttt_quit(ttt_game_dictionary[ctx.channel], ctx)
-            return
+            return mongo_db.games.insert_one(self.game_doc)
         if 0 not in tt_list:
             await ctx.channel.send('It\'s a tie!')
             await Tictactoe.ttt_quit(ttt_game_dictionary[ctx.channel], ctx)
+            self.game_doc['winner'] = 'Tie!'
+            return mongo_db.games.insert_one(self.game_doc)
 
-    @commands.command(name='ttsetup', help='Customize the marker you place on the board. Type bot after to randomize the bot marker.')
+    @commands.command(name='ttsetup',
+                      help='Customize the marker you place on the board. Type bot after to randomize the bot marker.')
     async def ttt_game_setup(self, ctx, message='not bot'):
 
         def ttt_get_emoji(emoji_response):
-            return ctx.channel == emoji_response.channel and ctx.message.author == emoji_response.author and\
+            return ctx.channel == emoji_response.channel and ctx.message.author == emoji_response.author and \
                    emojis.count(emoji_response.content) > 0
 
         if ctx.message.author in ttt_game_dictionary[ctx.channel][1:3]:
@@ -236,13 +272,16 @@ class Tictactoe(commands.Cog):
 
     @commands.command(name='tt', help="To mark a square, type >tt # according to the grid.")
     async def ttt_mark_square(self, ctx, message):
-        if ctx.message.author == ttt_game_dictionary[ctx.channel][1] or ctx.message.author == ttt_game_dictionary[ctx.channel][2]:
+        if ctx.message.author == ttt_game_dictionary[ctx.channel][1] or ctx.message.author == \
+                ttt_game_dictionary[ctx.channel][2]:
             if int(message.strip()) in range(1, 10):
                 if ttt_game_dictionary[ctx.channel][0].tt_board_list[int(message.strip()) - 1] == 0:
-                    if ctx.message.author == ttt_game_dictionary[ctx.channel][1] and ttt_game_dictionary[ctx.channel][0].ttt_first_player == 1:
+                    if ctx.message.author == ttt_game_dictionary[ctx.channel][1] and ttt_game_dictionary[ctx.channel][
+                        0].ttt_first_player == 1:
                         ttt_game_dictionary[ctx.channel][0].tt_board_list[int(message.strip()) - 1] = 1
                         ttt_game_dictionary[ctx.channel][0].ttt_first_player = 2
-                    elif ctx.message.author == ttt_game_dictionary[ctx.channel][2] and ttt_game_dictionary[ctx.channel][0].ttt_first_player == 2:
+                    elif ctx.message.author == ttt_game_dictionary[ctx.channel][2] and ttt_game_dictionary[ctx.channel][
+                        0].ttt_first_player == 2:
                         ttt_game_dictionary[ctx.channel][0].tt_board_list[int(message.strip()) - 1] = 2
                         ttt_game_dictionary[ctx.channel][0].ttt_first_player = 1
                     # prevent unnecessary board in single player games.
@@ -255,7 +294,8 @@ class Tictactoe(commands.Cog):
             else:
                 await ctx.channel.send('Send a number between 1 and 9 to mark a square!')
         if ttt_game_dictionary[ctx.channel][0] is not None:
-            if ttt_game_dictionary[ctx.channel][2] is f'{bot.user.name}' and 0 in ttt_game_dictionary[ctx.channel][0].tt_board_list and ttt_game_dictionary[ctx.channel][0].ttt_first_player == 2:
+            if ttt_game_dictionary[ctx.channel][2] is f'{bot.user.name}' and 0 in ttt_game_dictionary[ctx.channel][
+                0].tt_board_list and ttt_game_dictionary[ctx.channel][0].ttt_first_player == 2:
                 await Tictactoe.ttt_ai_move(ttt_game_dictionary[ctx.channel][0], ctx)
                 ttt_game_dictionary[ctx.channel][0].ttt_first_player = 1
                 await Tictactoe.ttt_output(ttt_game_dictionary[ctx.channel][0], ctx)
@@ -263,11 +303,10 @@ class Tictactoe(commands.Cog):
 
 @bot.command(name='ttstart', help='Starts a game of tic tac toe.')
 async def ttt_start(ctx):
-
     def ttt_get_players(num_response):
         # Check to make sure the game starter is answering the question.
         return ctx.message.author == num_response.author and ctx.channel == num_response.channel and \
-               num_response.content.lower().strip() in ['1', '2', 'one', 'two', '1p', '2p']
+               num_response.content.lower().strip() in ['1', '2', 'one', 'two', '1p', '2p', '>tt 1', '>tt 2']
 
     def ttt_get_second_player(me_response):
         return ctx.channel == me_response.channel and ttt_player_one != me_response.author and \
@@ -280,11 +319,11 @@ async def ttt_start(ctx):
         await ctx.channel.send('1p and 2p games are allowed. How many players are there?')
         while ttt_players == 0:
             response = await bot.wait_for('message', check=ttt_get_players)
-            if response.content.lower().strip() in ['1', 'one', '1p']:
+            if response.content.lower().strip() in ['1', 'one', '1p', '>tt 1']:
                 ttt_player_one = response.author
                 ttt_player_two = f'{bot.user.name}'
                 ttt_players = 1
-            elif response.content.lower().strip() in ['2', 'two', '2p']:
+            elif response.content.lower().strip() in ['2', 'two', '2p', '>tt 2']:
                 ttt_player_one = response.author
                 await ctx.channel.send('Who is the second player? Type "me".')
                 ttt_player_two = 0
@@ -335,11 +374,18 @@ class Hangman(commands.Cog):
 
     from module_vars import hangman_vocab, hangman_list
 
-    def __init__(self):
+    def __init__(self, ctx):
         self.hangman_image_counter = 0
         self.word = random.choice(self.hangman_vocab)
         self.used_word_list = []
         Hangman.hangman_guess_list(self)
+
+        today_date = date.today()
+        self.game_doc = {'Server': ctx.guild.name,
+                         'game': 'Hangman',
+                         'guesses': 0, 'date': '%s/%s/%s' % (today_date.month,
+                                                             today_date.day,
+                                                             today_date.year)}
 
     # The list that will be used to display the current state of the guessed word is self.guess_list. _ for blanks,
     # letters for correct guesses, and any non-letter characters will already be displayed. self.guess is for
@@ -353,7 +399,6 @@ class Hangman(commands.Cog):
                 self.guess_list.append(item)
         Hangman.hangman_guess_filler(self)
 
-
     def hangman_guess_filler(self):
         self.guess = '```'
         for item in self.guess_list:
@@ -362,7 +407,8 @@ class Hangman(commands.Cog):
                 self.guess += " "
         self.guess += '```'
 
-    @commands.command(name='hmword', help='Submit a custom word in spoiler tags, ex. ||word||. This will restart the game.')
+    @commands.command(name='hmword',
+                      help='Submit a custom word in spoiler tags, ex. ||word||. This will restart the game.')
     async def hangman_custom(self, ctx, *, message):
         hangman_custom_word = ''
         if re.search("[a-zA-Z]", message) is not None:
@@ -393,31 +439,41 @@ class Hangman(commands.Cog):
 
     async def hangman_wrong(self, ctx):
         hm_game_dictionary[ctx.channel].hangman_image_counter += 1
+        self.game_doc['guesses'] = f'{hm_game_dictionary[ctx.channel].hangman_image_counter}'
         await Hangman.hangman_output(hm_game_dictionary[ctx.channel], ctx.message)
         if hm_game_dictionary[ctx.channel].hangman_image_counter == 6:
             bot_message = f'Your man is hung. RIP. The word was {hm_game_dictionary[ctx.channel].word[0].lower()}, ' \
                           f'which means {hm_game_dictionary[ctx.channel].word[1]}.'
             await ctx.channel.send(bot_message)
+            self.game_doc['status'] = 'Hung'
+            self.game_doc['lastplayer'] = ctx.message.author.name + "#" + ctx.message.author.discriminator
             hm_game_dictionary.pop(ctx.channel, None)
+            return mongo_db.games.insert_one(self.game_doc)
 
     async def hangman_win(self, ctx):
         bot_message = f'Congratulations! You won. The word was {hm_game_dictionary[ctx.channel].word[0].lower()}, ' \
                       f'which means {hm_game_dictionary[ctx.channel].word[1]}.'
         await ctx.channel.send(bot_message)
+        self.game_doc['status'] = 'Survived'
+        self.game_doc['lastplayer'] = ctx.message.author.name + "#" + ctx.message.author.discriminator
         hm_game_dictionary.pop(ctx.channel, None)
+        return mongo_db.games.insert_one(self.game_doc)
 
-    @commands.command(name='hm', help='Make a guess. Use single letters unless you are confident you know the entire word!')
+    @commands.command(name='hm',
+                      help='Make a guess. Use single letters unless you are confident you know the entire word!')
     async def hangman_check(self, ctx, message):
         if message.strip().isalpha() is False:
             await ctx.channel.send('Use a letter.')
         else:
-            if len(message.strip()) == 1 and message.strip().lower() not in hm_game_dictionary[ctx.channel].used_word_list:
+            if len(message.strip()) == 1 and message.strip().lower() not in hm_game_dictionary[
+                ctx.channel].used_word_list:
                 hm_game_dictionary[ctx.channel].used_word_list.append(message.strip().lower())
                 hm_letter_counter = 0
                 for letter in range(0, len(hm_game_dictionary[ctx.channel].word[0])):
                     if message[-1].strip().lower() == hm_game_dictionary[ctx.channel].word[0][letter].lower():
                         hm_letter_counter += 1
-                        hm_game_dictionary[ctx.channel].guess_list[letter] = hm_game_dictionary[ctx.channel].word[0][letter].upper()
+                        hm_game_dictionary[ctx.channel].guess_list[letter] = hm_game_dictionary[ctx.channel].word[0][
+                            letter].upper()
                     else:
                         pass
                 if hm_letter_counter == 0:
@@ -430,7 +486,8 @@ class Hangman(commands.Cog):
                     return
                 if hm_game_dictionary[ctx.channel].word[0].lower() == message.strip().lower():
                     for letter in range(0, len(hm_game_dictionary[ctx.channel].word[0])):
-                        hm_game_dictionary[ctx.channel].guess_list[letter] = hm_game_dictionary[ctx.channel].word[0][letter].upper()
+                        hm_game_dictionary[ctx.channel].guess_list[letter] = hm_game_dictionary[ctx.channel].word[0][
+                            letter].upper()
                     await Hangman.hangman_output(hm_game_dictionary[ctx.channel], ctx.message)
                     await Hangman.hangman_win(hm_game_dictionary[ctx.channel], ctx)
                 else:
@@ -450,19 +507,19 @@ class Hangman(commands.Cog):
 
 
 @bot.command(name='hmstart', help='Starts a game of hangman.')
-async def hangman_start(message):
-    if message.channel not in hm_game_dictionary:
-        hm_game_dictionary[message.channel] = Hangman()
-        await message.channel.send('Type >help to see a list of commands available for hangman.')
-        await Hangman.hangman_output(hm_game_dictionary[message.channel], message)
+async def hangman_start(ctx):
+    if ctx.channel not in hm_game_dictionary:
+        hm_game_dictionary[ctx.channel] = Hangman(ctx)
+        await ctx.channel.send('Type >help to see a list of commands available for hangman.')
+        await Hangman.hangman_output(hm_game_dictionary[ctx.channel], ctx.message)
     else:
-        await message.channel.send('There is already a hangman game in this channel!')
-    bot.add_cog(Hangman())
+        await ctx.channel.send('There is already a hangman game in this channel!')
+    bot.add_cog(Hangman(ctx))
 
 
 @bot.event
 async def on_ready():
-    ready_message = f'{bot.user.name} is here! Type ">help" for a basic command list.'
+    ready_message = f'{bot.user.name} just woke up! Type ">help" for a basic command list.'
     channel = discord.utils.get(bot.get_all_channels(), name='playfriend')
     await channel.send(ready_message)
 
