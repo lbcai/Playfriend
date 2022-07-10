995081b1968bdfa919eb754804aaa2d5cbeb9e9b
L Cai <lizbcai@gmail.com>
2022-07-09 20:45:05 -0700

    Added animations to nav bar, main logo. Changed button colors.

--- react-flask-app/src/App.js
+++ react-flask-app/src/App.js
@@ -1,7 +1,11 @@
 /* import { Route, Switch } from "react-router-dom"; */
 import NavBar from './NavBar';
 import './App.css';
-import icon from './images/pf_icon_vector.svg';
+import logoCircle from './images/pf_icon_complete.svg';
+import textShort from './images/pf_text_short.svg';
+import textLong from './images/pf_text.svg';
+import 'animate.css';
+
 
 function App() {
   return (
@@ -9,17 +13,20 @@ function App() {
 
       <NavBar />
 
+      <div className='animate__animated splat hero'>
+        <img className='animate__animated animate__rollIn animate__faster App-logo' src={ logoCircle } alt="Playfriend Logo" />
+        <img className='animate__animated textPop App-logo-text' src={ textShort } alt="Playfriend Logo" />
+        <img className='animate__animated mask animate__faster App-logo-text' src={ textLong } alt="Playfriend Logo" />
+      </div>
 
-
-      <div className='hero'>
-        <img className='App-logo' src={ icon } alt="Playfriend Logo" />
-        <header className="App-header">
+      <div className='App-header-div'>
+      <header className="App-header">
           A bot for chatroom minigames!
         </header>
         <button> Invite to Server </button>
       </div>
 
-      <div>
+      <div className='content'>
         <p>Text text here try now demo </p>
         <button> Demo </button>
         <p>test</p>
--- react-flask-app/src/NavBar.js
+++ react-flask-app/src/NavBar.js
@@ -5,31 +5,29 @@ import icon from './images/pf_icon_vector.svg';
 function NavBar() {
   return (
     <nav className='navBackground'>
-        <div className='navDiv'>
-        <img className='logo' src={ icon } alt="Playfriend Logo" />
-        </div>
 
-        <div className='navDiv'>
-            <NavLink
-                className={({ isActive }) =>
-                isActive ? 'active' : 'inactive'
-                }
-                exact="true" to="/features">Features</NavLink>
-        </div>
-        <div className='navDiv'>
-            <NavLink
-                className={({ isActive }) =>
-                isActive ? 'active' : 'inactive'
-                }
-                exact="true" to="/log">Game Log</NavLink>
-        </div>
-        <div className='navDiv'>
-            <NavLink
-                className={({ isActive }) =>
-                isActive ? 'active' : 'inactive'
-                }
-                exact="true" to="/contact">Contact</NavLink>
-        </div>
+        <img className='logo' src={ icon } alt="Playfriend Logo" />
+
+        <NavLink
+            className={({ isActive }) =>
+            isActive ? 'active navBox': 'inactive navBox'}
+            exact="true" to="/features">Features</NavLink>
+
+        <NavLink
+            className={({ isActive }) =>
+            isActive ? 'active navBox': 'inactive navBox'}
+            exact="true" to="/log">Game Log</NavLink>
+
+        <NavLink
+            className={({ isActive }) =>
+            isActive ? 'active navBox': 'inactive navBox'}
+            exact="true" to="/manual">Manual</NavLink>
+
+        <NavLink
+            className={({ isActive }) =>
+            isActive ? 'active navBox': 'inactive navBox'}
+            exact="true" to="/contact">Contact</NavLink>
+
     </nav>
   );
 }
