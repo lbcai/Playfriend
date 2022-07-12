94692d3af6ddde6b3b5a791e71019ae70cf9af19
L Cai <lizbcai@gmail.com>
2022-07-11 23:42:44 -0700

    Add drop shadows, main content box styling. Intro content.

--- react-flask-app/src/Main.js
+++ react-flask-app/src/Main.js
@@ -3,6 +3,15 @@ import textShort from './images/pf_text_short.svg';
 import textLong from './images/pf_text.svg';
 
 function Main() {
+
+    const handleClick_invite = () => {
+        window.open("https://discord.com/oauth2/authorize?client_id=785345529722175498&permissions=470080&scope=bot");
+      };
+
+    const handleClick_demo = () => {
+    window.open("https://discord.com/oauth2/authorize?client_id=785345529722175498&permissions=470080&scope=bot");
+    };
+
     return (
         <div>
             <div className='animate__animated splat hero'>
@@ -15,36 +24,36 @@ function Main() {
             <header className="App-header">
                 A bot for chatroom minigames!
                 </header>
-                <button> Invite to Server </button>
+                <button onClick={handleClick_invite}> Invite to Server </button>
             </div>
 
             <div className='content'>
-                <p>Text text here try now demo </p>
-                <button> Demo </button>
-                <p>test</p>
-                <p>test</p>
-                <p>test</p>
-                <p>test</p>
-                <p>test</p>
-                <p>test</p>
-                <p>test</p>
-                <p>test</p>
-                <p>test</p>
-                <p>test</p>
-                <p>test</p>
-                <p>test</p>
-                <p>test</p>
-                <p>test</p>
-                <p>test</p>
-                <p>test</p>
-                <p>test</p>
-                <p>test</p>
-                <p>test</p>
-                <p>test</p>
-                <p>test</p>
-                <p>test</p>
-                <p>test</p>
+                <div className='content-div-left'>
+                    <p>Playfriend is a chat bot that allows members of your server to play minigames together.
+                        New minigames are currently in development and feature suggestions are welcome.</p>
+                    <p>Playfriend will also include random generators, helpful chat features, and more.</p>
+
+                    <p>Try out Playfriend with a demo!</p>
+                    <button onClick={handleClick_demo}> Demo </button>
+                </div>
+
+                <div className='content-div-left'>
+                    <p>test</p>
+                </div>
             </div>
+
+            <div className='content'>
+                <p>Features</p>
+            </div>
+
+            <div className='content'>
+                <p>Game Log</p>
+            </div>
+
+            <div className='content'>
+                <p>Contact</p>
+            </div>
+
         </div>
     );
 }
